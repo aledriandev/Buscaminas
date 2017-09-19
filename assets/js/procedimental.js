4f1ccb45eef3c3a414b7227b95ffebeb90bfed56
AQP20171679 <alejandraadriantejada@gmail.com>
2017-09-19 10:34:33 -0500

    Cuenta y muestra bombas

--- assets/js/procedimental.js
+++ assets/js/procedimental.js
@@ -1,17 +1,17 @@
 'use strict';
 
 let bombs = 10;
-const colSize = 9; //tamaño horizontal
-const rowSize = 12; // tamaño vertical
+const rowSize = 7; // tamaño vertical
 let grid = [];
 // Cotruccion de tabla
 let tableMines = $('#tableMines');
 for( let row=0; row<rowSize; row++ ){
     let tr = document.createElement('tr');
     grid[row] = [];
-    for( let col=0; col<colSize; col++ ){
+    for( let col=0; col<rowSize; col++ ){
         let td = document.createElement('td');
             td.setAttribute('class','square');
+            td.setAttribute('id',row+'-'+col);
         grid[row][col] = {
             hasBomb: false, 
             bombCount: 0
@@ -22,20 +22,34 @@ for( let row=0; row<rowSize; row++ ){
 }
 
 while(bombs> 0){
-    var x = Math.ceil(Math.random() * rowSize);
-    var y = Math.ceil(Math.random() * colSize);
+    let x = Math.floor(Math.random()*5)+1
+    let y = Math.floor(Math.random()*5)+1
+    // console.log(x);
+    // console.log(y);
     if ( !(grid[x][y]).hasBomb ) {
         (grid[x][y]).hasBomb = true;
+        console.log(grid[x][y]);
         // (grid[x][y]).setAttribute ('class','bomb');
         bombs--;
+        console.log(bombs);
+    }
+}
+//colocar las bombas
+for (var i = 0; i < rowSize; i++) {
+    for (var j = 0; j < rowSize; j++) {
+        if ( (grid[i][j]).hasBomb ) {
+            console.log(i +'-'+ j)
+            $(`#${i}-${j}`).addClass('bombHere');
+        }
     }
 }
 
-for (var i = 1; i <= gridSize; i++) {
-    for (var j = 1; j <= this.gridSize; j++) {
-
+//sumar cuando esta en el centro
+for (var i = 1; i < rowSize-1; i++) {
+    for (var j = 1; j < rowSize-1; j++) {
         if ( (grid[i][j]).hasBomb ) {
-
+            console.log(i +'-'+ j)
+            // $(`#${i}-${j}`).addClass('bombHere');
             grid[i-1][j-1].bombCount++;
             grid[i-1][j  ].bombCount++;
             grid[i-1][j+1].bombCount++;
@@ -44,7 +58,20 @@ for (var i = 1; i <= gridSize; i++) {
             grid[i+1][j-1].bombCount++;
             grid[i+1][j  ].bombCount++;
             grid[i+1][j+1].bombCount++;
-
         }
     }
+}
+
+for (var i = 0; i < rowSize; i++) {
+    for (var j = 0; j < rowSize; j++) {
+        let number = grid[i][j].bombCount;
+        let hasBombHere = grid[i][j].hasBomb;
+        if (number>0 && !hasBombHere){
+            $(`#${i}-${j}`).text(number);
+        }
+    }
+}
+
+function numRandom(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
 }
