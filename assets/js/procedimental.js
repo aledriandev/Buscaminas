5513321755b087c0b917364d9aa18fc929e1823d
AQP20171679 <alejandraadriantejada@gmail.com>
2017-09-19 11:26:39 -0500

    numeros correctos

--- assets/js/procedimental.js
+++ assets/js/procedimental.js
@@ -12,24 +12,26 @@ for( let row=0; row<rowSize; row++ ){
         let td = document.createElement('td');
             td.setAttribute('class','square');
             td.setAttribute('id',row+'-'+col);
+        let divTd = document.createElement('div');
+            divTd.setAttribute('class','hidde-td');
         grid[row][col] = {
             hasBomb: false, 
             bombCount: 0
         };
+        td.appendChild(divTd);
         tr.appendChild(td);
     }
     tableMines.append(tr);
 }
 
 while(bombs> 0){
-    let x = Math.floor(Math.random()*5)+1
-    let y = Math.floor(Math.random()*5)+1
+    let x = Math.floor(Math.random()*6)
+    let y = Math.floor(Math.random()*6)
     // console.log(x);
     // console.log(y);
     if ( !(grid[x][y]).hasBomb ) {
         (grid[x][y]).hasBomb = true;
         console.log(grid[x][y]);
-        // (grid[x][y]).setAttribute ('class','bomb');
         bombs--;
         console.log(bombs);
     }
@@ -44,20 +46,63 @@ for (var i = 0; i < rowSize; i++) {
     }
 }
 
-//sumar cuando esta en el centro
-for (var i = 1; i < rowSize-1; i++) {
-    for (var j = 1; j < rowSize-1; j++) {
+//colocar los numeros
+for (var i = 0; i < rowSize; i++) {
+    for (var j = 0; j < rowSize; j++) {
         if ( (grid[i][j]).hasBomb ) {
-            console.log(i +'-'+ j)
-            // $(`#${i}-${j}`).addClass('bombHere');
-            grid[i-1][j-1].bombCount++;
-            grid[i-1][j  ].bombCount++;
-            grid[i-1][j+1].bombCount++;
-            grid[i  ][j-1].bombCount++;
-            grid[i  ][j+1].bombCount++;
-            grid[i+1][j-1].bombCount++;
-            grid[i+1][j  ].bombCount++;
-            grid[i+1][j+1].bombCount++;
+            if( i==0 && j==0){//primera esquina
+                grid[i  ][j+1].bombCount++;
+                grid[i+1][j  ].bombCount++;
+                grid[i+1][j+1].bombCount++;
+            } else if( i==0 && j==6){//segunda esquina
+                grid[i  ][j-1].bombCount++;
+                grid[i+1][j-1].bombCount++;
+                grid[i+1][j  ].bombCount++;
+            } else if( i==6 && j==0){//tercera esquina
+                grid[i  ][j+1].bombCount++;
+                grid[i-1][j  ].bombCount++;
+                grid[i-1][j+1].bombCount++;
+            } else if( i==6 && j==6){//cuarta esquina
+                grid[i  ][j-1].bombCount++;
+                grid[i-1][j-1].bombCount++;
+                grid[i-1][j  ].bombCount++;
+            } else if( i==0 && (j!=0 || j!=6 )){ //lateral i==0
+                grid[i+1][j-1].bombCount++;
+                grid[i+1][j  ].bombCount++;
+                grid[i+1][j+1].bombCount++;
+                grid[i  ][j-1].bombCount++;
+                grid[i  ][j+1].bombCount++;
+            } else if( i==6 && (j!=0 || j!=6 )){ //lateral 6==0
+                grid[i-1][j-1].bombCount++;
+                grid[i-1][j  ].bombCount++;
+                grid[i-1][j+1].bombCount++;
+                grid[i  ][j-1].bombCount++;
+                grid[i  ][j+1].bombCount++;
+            } else if(j==0 && (i!=0 || i!=6 ) ){ //lateral j==0
+                grid[i  ][j+1].bombCount++;
+                grid[i-1][j+1].bombCount++;
+                grid[i+1][j+1].bombCount++;
+                grid[i-1][j  ].bombCount++;
+                grid[i+1][j  ].bombCount++;
+            } else if(j==6 && (i!=0 || i!=6 ) ){ //lateral j==0
+                grid[i  ][j-1].bombCount++;
+                grid[i-1][j-1].bombCount++;
+                grid[i+1][j-1].bombCount++;
+                grid[i-1][j  ].bombCount++;
+                grid[i+1][j  ].bombCount++;
+            }
+            else{
+                console.log(i +'-'+ j)
+                // $(`#${i}-${j}`).addClass('bombHere');
+                grid[i-1][j-1].bombCount++;
+                grid[i-1][j  ].bombCount++;
+                grid[i-1][j+1].bombCount++;
+                grid[i  ][j-1].bombCount++;
+                grid[i  ][j+1].bombCount++;
+                grid[i+1][j-1].bombCount++;
+                grid[i+1][j  ].bombCount++;
+                grid[i+1][j+1].bombCount++;
+            }
         }
     }
 }
